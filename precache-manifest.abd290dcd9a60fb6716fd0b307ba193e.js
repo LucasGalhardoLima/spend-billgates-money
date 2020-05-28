9129614ddc8d289351e2fe2f2724813f5642a79a
Lucas Galhardo Lima <lucas.galhardo@pushin.com.br>
2020-05-28 17:45:27 -0300

    Initial dist subtree commit

---
+++ precache-manifest.abd290dcd9a60fb6716fd0b307ba193e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce3514a473b176aa991",
+    "url": "/spend-billgates-money/css/app.caff07a8.css"
+  },
+  {
+    "revision": "9d2f569b5777f776b01d75cebc3fa819",
+    "url": "/spend-billgates-money/img/billgates.9d2f569b.png"
+  },
+  {
+    "revision": "8fda47654e52056bd7433111fb3c73b7",
+    "url": "/spend-billgates-money/index.html"
+  },
+  {
+    "revision": "7ce3514a473b176aa991",
+    "url": "/spend-billgates-money/js/app.8264d750.js"
+  },
+  {
+    "revision": "378d5b54cf635cb47e50",
+    "url": "/spend-billgates-money/js/chunk-vendors.243256a0.js"
+  },
+  {
+    "revision": "c635c2806fbabe7959ef9515c779dfd2",
+    "url": "/spend-billgates-money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/spend-billgates-money/robots.txt"
+  }
+]);
